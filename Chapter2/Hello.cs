8fa2317c4ad712d4b43e8b0dcbc488af3ef3f14e
DragonT13 <alex23456@naver.com>
2021-07-13 18:11:36 +0900

    07/13

---
+++ Chapter2/Hello.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Chapter2
+{
+    class Hello
+    {
+        //프로그램 실행이 시작되는 곳
+        public static void PrintHello(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
---
+++ Chapter2/MainApp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chapter2
+{
+    class MainApp
+    {
+        
+        static void Main(string[] args)
+        {
+            Hello.PrintHello(args);
+        }
+    }
+}
